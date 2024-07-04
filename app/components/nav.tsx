9d7d3acf559e77821535c644c1c4cdba341ec163
Maggie Hillebrecht <70683116+MagPie0@users.noreply.github.com>
2024-07-04 11:34:47 -0700

    Update navagation
    need to connect a new site to projects tab

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -5,10 +5,10 @@ const navItems = {
     name: 'home',
   },
   '/blog': {
-    name: 'blog',
+    name: 'portfolio',
   },
   'https://vercel.com/templates/next.js/portfolio-starter-kit': {
-    name: 'deploy',
+    name: 'projects',
   },
 }
 
