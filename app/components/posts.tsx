a1cc82ca310658470eba8c52d0d3c0cfaa4a1ec8
Maggie Hillebrecht <70683116+MagPie0@users.noreply.github.com>
2024-07-04 19:49:35 -0700

    fixing

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -1,39 +1,36 @@
-import Link from 'next/link';
-import { formatDate, getBlogPosts } from 'app/blog/utils';
+import Link from 'next/link'
+import { formatDate, getBlogPosts } from 'app/blog/utils'
 
 export function BlogPosts() {
-  let allBlogs = getBlogPosts();
+  let allBlogs = getBlogPosts()
 
   return (
     <div>
       {allBlogs
-        .sort((a, b) => new Date(b.metadata.publishedAt) - new Date(a.metadata.publishedAt))
+        .sort((a, b) => {
+          if (
+            new Date(a.metadata.publishedAt) > new Date(b.metadata.publishedAt)
+          ) {
+            return -1
+          }
+          return 1
+        })
         .map((post) => (
           <Link
             key={post.slug}
-            href={`/blog/${post.slug}`}
-            passHref >
-            <a className="flex flex-col space-y-1 mb-4">
-              <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2 items-center">
-                <p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums">
-                  {formatDate(post.metadata.publishedAt, false)}
-                </p>
-                <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
-                  {post.metadata.title}
-                </p>
-                <div className="flex flex-wrap gap-1">
-                  {post.metadata.tags.map((tag) => (
-                    <span
-                      key={tag}
-                      className="text-xs px-2 py-1 bg-gray-200 text-gray-800 rounded-md">
-                      {tag}
-                    </span>
-                  ))}
-                </div>
-              </div>
-            </a>
+            className="flex flex-col space-y-1 mb-4"
+            href={/blog/${post.slug}}
+          >
+            <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
+              <p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums">
+                {formatDate(post.metadata.publishedAt, false)}
+              </p>
+              <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
+                {post.metadata.title}
+              </p>
+            </div>
           </Link>
         ))}
     </div>
-  );
+  )
 }
