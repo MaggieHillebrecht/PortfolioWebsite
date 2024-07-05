bcfc8ccddc6b582f97cdb4db8047272e455eeccf
Maggie Hillebrecht <70683116+MagPie0@users.noreply.github.com>
2024-07-04 17:49:39 -0700

    adding break

--- app/page.tsx
+++ app/page.tsx
@@ -14,8 +14,8 @@ export default function Page() {
         />
       </h1>
       <p className="mb-4">
-        {`I am a student at Chapman University and currently majoring in Computer Science with a minor in Game Development Programming. <br />
-          I found my passion for game development and programming through making Cosmic Breakfast Adventures when I was in 8th grade using scratch programming.`}
+        I am a student at Chapman University and currently majoring in Computer Science with a minor in Game Development Programming. <br />
+        I found my passion for game development and programming through making Cosmic Breakfast Adventures when I was in 8th grade using scratch programming.
       </p>
       <div className="my-8">
         <BlogPosts />
