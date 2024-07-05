b3ebe50cd9e68aae568cff8eb4c9e36d9e486719
Maggie Hillebrecht <70683116+MagPie0@users.noreply.github.com>
2024-07-04 17:48:02 -0700

    Polishing home page more

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Page() {
         />
       </h1>
       <p className="mb-4">
-        {`I am a student at Chapman University and currently majoring in Computer Science with a minor in Game Development Programming.
+        {`I am a student at Chapman University and currently majoring in Computer Science with a minor in Game Development Programming. <br />
           I found my passion for game development and programming through making Cosmic Breakfast Adventures when I was in 8th grade using scratch programming.`}
       </p>
       <div className="my-8">
