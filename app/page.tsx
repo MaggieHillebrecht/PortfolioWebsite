626e0c49ba7dee7bd21ed875e20660fcdc2957f1
Maggie Hillebrecht <70683116+MagPie0@users.noreply.github.com>
2024-07-04 17:53:00 -0700

    removing blog post look in home page

--- app/page.tsx
+++ app/page.tsx
@@ -17,9 +17,6 @@ export default function Page() {
         I am a student at Chapman University and currently majoring in Computer Science with a minor in Game Development Programming. <br /> <br />
         I found my passion for game development and programming through making Cosmic Breakfast Adventures when I was in 8th grade using scratch programming.
       </p>
-      <div className="my-8">
-        <BlogPosts />
-      </div>
     </section>
   )
 }
