9866e6bce0e9f797408dc6fd4119196b641d06e6
Maggie Hillebrecht <70683116+MagPie0@users.noreply.github.com>
2024-07-05 20:35:08 -0700

    debug

--- app/Portfolio/page.tsx
+++ app/Portfolio/page.tsx
@@ -1,4 +1,4 @@
-import { BlogPosts } from 'app/components/posts'
+import { ProjectPost } from 'app/components/posts'
 
 export const metadata = {
   title: 'Portfolio',
--- app/future_projects/page.tsx
+++ app/future_projects/page.tsx
@@ -1,4 +1,4 @@
-import { BlogPosts } from 'app/components/posts'
+import { FutureProjectPosts } from 'app/components/posts'
 
 export const metadata = {
   title: 'Future Projects',
