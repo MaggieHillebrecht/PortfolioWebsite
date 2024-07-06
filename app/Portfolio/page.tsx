c998a3ae85a7ec587e1c721f1952ab2ac448718b
Maggie Hillebrecht <70683116+MagPie0@users.noreply.github.com>
2024-07-05 21:06:54 -0700

    sending out to the customer

--- app/Portfolio/page.tsx
+++ app/Portfolio/page.tsx
@@ -10,7 +10,9 @@ export default function Page() {
     <section>
       <h1 className="font-semibold text-2xl mb-8 tracking-tighter">My Portfolio</h1>
       <p>Here are games that I have worked on:</p>
+      <div style={{ marginTop: '1rem' }}>
       <BlogPosts />
+      </div>
     </section>
   )
 }
