99f94abffb94c4667a4e70cafb9a67b4f69c90ae
Maggie Hillebrecht <70683116+MagPie0@users.noreply.github.com>
2024-07-05 18:19:07 -0700

    Creating folders for the new tabs

---
+++ app/future projects/page.tsx
@@ -0,0 +1,16 @@
+import { BlogPosts } from 'app/components/posts'
+
+export const metadata = {
+  title: 'Portfolio',
+  description: 'View my Game Portfolio',
+}
+
+export default function Page() {
+  return (
+    <section>
+      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">My Portfolio</h1>
+      <p>Here are games that I have worked on:</p>
+      <BlogPosts />
+    </section>
+  )
+}
