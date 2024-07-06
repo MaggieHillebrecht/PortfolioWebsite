aeb9c8463a0f3b54e2176bd0f8fc11921bd2cd4b
Maggie Hillebrecht <70683116+MagPie0@users.noreply.github.com>
2024-07-05 22:06:51 -0700

    He is eating it

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,30 +1,17 @@
-import { getBlogPosts } from 'app/Portfolio/utils';
-import { getProjects } from 'app/projects/utils';
-import { getFutureProjects } from 'app/future_projects/utils';
+import { getBlogPosts } from 'app/Portfolio/utils'
 
-export const baseUrl = 'https://portfolio-blog-starter.vercel.app';
+export const baseUrl = 'https://portfolio-blog-starter.vercel.app'
 
-export async function generateSitemapData() {
+export default async function sitemap() {
   let blogs = getBlogPosts().map((post) => ({
     url: `${baseUrl}/blog/${post.slug}`,
     lastModified: post.metadata.publishedAt,
-  }));
+  }))
 
-  let projects = getProjects().map((project) => ({
-    url: `${baseUrl}/projects/${project.slug}`,
-    lastModified: project.metadata.publishedAt,
-  }));
-
-  let futureProjects = getFutureProjects().map((project) => ({
-    url: `${baseUrl}/future-projects/${project.slug}`,
-    lastModified: project.metadata.publishedAt,
-  }));
-
-  let routes = ['', '/blog', '/projects', '/future_projects'].map((route) => ({
+  let routes = ['', '/blog'].map((route) => ({
     url: `${baseUrl}${route}`,
     lastModified: new Date().toISOString().split('T')[0],
-  }));
+  }))
 
-  return [...routes, ...blogs, ...projects, ...futureProjects];
+  return [...routes, ...blogs]
 }
-
