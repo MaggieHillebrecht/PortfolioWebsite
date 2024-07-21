7a8f0fd6b029382a278964aecc902552860b18fd
Maggie Hillebrecht <70683116+MagPie0@users.noreply.github.com>
2024-07-20 17:18:49 -0700

    please work

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    theme: {
+      extend: {
+        backgroundColor: {
+          'white-75': 'rgba(255, 255, 255, 0.75)',
+          'gray-800-75': 'rgba(31, 41, 55, 0.75)',
+        },
+      },
+    },
+    variants: {
+      extend: {
+        backgroundColor: ['dark'],
+      },
+    },
+    plugins: [],
+  };
