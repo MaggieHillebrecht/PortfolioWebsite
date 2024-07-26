362b44954d4b9d44da0d1f83161b738c0437bbe0
Maggie Hillebrecht <70683116+MagPie0@users.noreply.github.com>
2024-07-26 09:50:44 -0700

    ocd

--- app/GamePortfolio/[slug]/page.tsx
+++ app/GamePortfolio/[slug]/page.tsx
@@ -88,7 +88,7 @@ export default function Blog({ params }) {
         <img
           src={post.metadata.image}
           alt="Image"
-          className="title-image w-40 h-26 object-cover mr-4"
+          className="title-image w-50 h-36 object-cover mr-4"
         />
       )}
       {/* Getting the title of the post */}
