e592f2cf30c433a08da7685e8aa9a346666a70de
Maggie Hillebrecht <70683116+MagPie0@users.noreply.github.com>
2024-07-26 09:29:28 -0700

    adding the image into the post for game portfolio

--- app/GamePortfolio/[slug]/page.tsx
+++ app/GamePortfolio/[slug]/page.tsx
@@ -82,6 +82,18 @@ export default function Blog({ params }) {
           }),
         }}
       />
+      <div className="title-container flex items-center">
+        {post.metadata.image && (
+          <img
+            src={post.metadata.image}
+            alt="Image"
+            className="title-image w-25 h-16 object-cover mr-4"
+          />
+        )}
+      <h1 className="title font-semibold text-2xl tracking-tighter">
+        {post.metadata.title}
+      </h1>
+      </div>
       <h1 className="title font-semibold text-2xl tracking-tighter">
         {post.metadata.title}
       </h1>
