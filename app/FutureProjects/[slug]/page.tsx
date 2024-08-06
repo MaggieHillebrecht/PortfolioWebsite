4caf7c823525a6eac6dbed421cb69eacdd0efccc
Maggie Hillebrecht <70683116+MagPie0@users.noreply.github.com>
2024-08-06 12:26:40 -0700

    ocd

--- app/FutureProjects/[slug]/page.tsx
+++ app/FutureProjects/[slug]/page.tsx
@@ -88,7 +88,7 @@ export function generateMetadata({ params }) {
             <img
               src={post.metadata.image}
               alt="Image"
-              className="title-image w-25 h-16 object-cover mr-4"
+              className="title-image w-50 h-30 object-cover mr-4"
             />
           )}
           <h1 className="title font-semibold text-2xl tracking-tighter">
